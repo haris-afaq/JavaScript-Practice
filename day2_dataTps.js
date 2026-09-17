d2a7c641d82a48b5663feac0df219f3c414293ac
haris afaq <haris.afaq@inara.pk>
2026-09-17 11:38:04 +0500

    "Data Types"

---
+++ day2_dataTps.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat the JavaScript code as Newer version
+
+//alert(3+3) it can be used in brower but we are using node js so it will gives an error
+
+/**
+ Data Types in Js:
+ Tells us what kind of data a variable has stored.
+ Following are the main data types in js
+
+ 1) Number => Used to store Numerical Values 
+ 2) String => Store Words like Name, address etc, and use ""
+ 3) Boolean => Use to store only 2 values i.e: True and False
+ 4) null => Stand alone value, that is the representation of empty value
+ 5) Undefined => When you don't defined the value of a variable after declaraing it
+ 6) symbol => Using it when we are creating a lot of components and commonly used in React
+ Symbol is commonly used for Uniqueness
+ 7) Objects => Will cover later
+
+
+
+ Note: for checking the data type of data we use type of method
+ */
+console.log(typeof false); // It will print Boolean as we have write typeof false.
+// We can also check the type of any var like
+
+let age = 24;
+console.log(typeof age);// It will return Number as the type of age var
